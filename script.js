792ce602db9bbdd0d3a6f748cf2c53aa4d5ac911
ralpmike <ralphmike996@gmail.com>
2023-09-29 12:37:25 +0100

    first commit

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
 let toggleBtn = document.querySelector('#toggler');
-let butns = document.querySelector(".buttons")
-let togglebox = document.querySelector('.toggle')
-let deleBtn = document.querySelector('.delbtn')
-let bgColor = document.querySelector('.cal-wrapper')
-let inputValue = document.querySelector('input')
-let hcolor = document.querySelector('h1')
-let buttons = document.querySelectorAll('button')
-let resetBtn = document.querySelector('.reset')
-let equalBtn = document.querySelector('#evaluate')
-let h3color = document.querySelector('h3')
-let h2color = document.querySelector('h2')
-let toggleBg =document.querySelector('.toggle')
-let toggler = document.querySelector('#toggler')
-
+let butns = document.querySelector(".buttons");
+let togglebox = document.querySelector('.toggle');
+let deleBtn = document.querySelector('.delbtn');
+let bgColor = document.querySelector('.cal-wrapper');
+let inputValue = document.querySelector('input');
+let hcolor = document.querySelector('h1');
+let buttons = document.querySelectorAll('button');
+let resetBtn = document.querySelector('.reset');
+let equalBtn = document.querySelector('#evaluate');
+let h3color = document.querySelector('h3');
+let h2color = document.querySelector('h2');
+let toggleBg =document.querySelector('.toggle');
+let toggler = document.querySelector('#toggler');
 
 function getValue(value) {
   inputValue.value += value
-  value.preventDefault()
+  value.preventDefault();
   valueReset()
- 
 }
 
 function displayResult(){
-  inputValue.value = eval(inputValue.value);
+  if(!inputValue.value){
+    inputValue.value = ''
+  }else {
+    inputValue.value = eval(inputValue.value);
+  }
 }
 
 function valueReset() {
@@ -73,17 +75,39 @@ function changeThemeOneBg() {
   hcolor.style.color = 'hsl(60, 10%, 19%)';
   deleBtn.style.color = 'white';
   deleBtn.style.backgroundColor ='hsl(185, 42%, 37%)';
+  deleBtn.addEventListener('mouseenter', () => {
+    deleBtn.style.backgroundColor ='hsl(185, 42%, 60%)';
+  })
+  deleBtn.addEventListener('mouseleave', () => {
+    deleBtn.style.backgroundColor ='hsl(185, 42%, 37%)';
+    })
   deleBtn.style.borderColor = ' hsl(185, 58%, 25%)';
   resetBtn.style.color = 'white';
   resetBtn.style.backgroundColor ='hsl(185, 42%, 37%)';
+
+  resetBtn.addEventListener('mouseenter', () => {
+    resetBtn.style.backgroundColor ='hsl(185, 42%, 60%)';
+  })
+
+  resetBtn.addEventListener('mouseleave', () => {
+    resetBtn.style.backgroundColor ='hsl(185, 42%, 37%)';
+  })
+ 
+ 
   resetBtn.style.borderColor = 'hsl(185, 58%, 25%)';
   equalBtn.style.color = 'white';
   equalBtn.style.backgroundColor = 'hsl(25, 98%, 40%)';
+  equalBtn.addEventListener('mouseleave', () => {
+    equalBtn.style.backgroundColor ='hsl(25, 98%, 40%)';
+  })
+  equalBtn.addEventListener('mouseenter', () => {
+    equalBtn.style.backgroundColor ='hsl(25, 98%, 60%';
+  })
   equalBtn.style.borderColor = 'hsl(25, 99%, 27%)';
   h3color.style.color = 'hsl(60, 10%, 19%)';
   h2color.style.color = 'hsl(60, 10%, 19%)';
   toggleBg.style.backgroundColor = 'hsl(0, 5%, 81%)';
-  toggler.style.backgroundColor = 'hsl(25, 99%, 27%)'
+  toggler.style.backgroundColor = 'hsl(25, 99%, 27%)';
 }
 
 
@@ -93,6 +117,12 @@ function changeThemeTwoBg() {
     item.style.color= 'hsl(52, 100%, 62%)';
     item.style.backgroundColor = 'hsl(268, 47%, 21%)'
     item.style.borderColor = 'hsl(290, 70%, 36%)'
+    item.addEventListener('mouseenter', () => {
+      item.style.backgroundColor = 'hsl(281, 89%, 26%)'
+    })
+    item.addEventListener('mouseleave', () => {
+      item.style.backgroundColor = 'hsl(268, 47%, 21%)'
+    })
   })
   butns.style.backgroundColor = 'hsl(268, 71%, 12%)';
   inputValue.style.backgroundColor = 'hsl(268, 71%, 12%)';
@@ -100,12 +130,33 @@ function changeThemeTwoBg() {
   hcolor.style.color = ' hsl(52, 100%, 62%)';
   deleBtn.style.color = 'white';
   deleBtn.style.backgroundColor ='hsl(281, 89%, 26%)';
+
+  deleBtn.addEventListener('mouseleave', () => {
+    deleBtn.style.backgroundColor ='hsl(281, 89%, 26%)';
+  })
+  deleBtn.addEventListener('mouseenter', () => {
+    deleBtn.style.backgroundColor =' hsla(281, 100%, 40%, 1)';
+  })
   deleBtn.style.borderColor = 'hsl(285, 91%, 52%)';
   resetBtn.style.color = 'white';
   resetBtn.style.backgroundColor =' hsl(281, 89%, 26%)';
+
+  resetBtn.addEventListener('mouseleave', () => {
+    resetBtn.style.backgroundColor =' hsl(281, 89%, 26%)';
+  })
+  resetBtn.addEventListener('mouseenter', () => {
+    resetBtn.style.backgroundColor =' hsla(281, 100%, 40%, 1)';
+  })
+
   resetBtn.style.borderColor = 'hsl(285, 91%, 52%)';
   equalBtn.style.color = ' hsl(198, 20%, 13%)';
   equalBtn.style.backgroundColor = 'hsl(176, 100%, 44%)';
+  equalBtn.addEventListener('mouseleave', () => {
+    equalBtn.style.backgroundColor = 'hsl(176, 100%, 44%)';
+  })
+  equalBtn.addEventListener('mouseenter', () => {
+    equalBtn.style.backgroundColor = 'hsla(176, 100%, 60%, 1)';
+  })
   equalBtn.style.borderColor = 'hsl(177, 92%, 70%)';
   h3color.style.color = ' hsl(52, 100%, 62%)';
   h2color.style.color = ' hsl(52, 100%, 62%)';
